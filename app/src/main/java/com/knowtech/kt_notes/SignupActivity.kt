06cbdf0fb1a955cb9d5baaa6474ef13bac8044c9
Ishant <ishantchauhan710@gmail.com>
2020-09-16 22:54:05 +0530

    First Commit
    Splash Screen
    Signup with Email and Password
    Signup with Google Account

---
+++ app/src/main/java/com/knowtech/kt_notes/SignupActivity.kt
@@ -0,0 +1,98 @@
+
+package com.knowtech.kt_notes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.knowtech.kt_notes.Constants.Companion.GOOGLE_SIGN_IN_REQUEST_CODE
+import kotlinx.android.synthetic.main.activity_signup.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class SignupActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        auth = FirebaseAuth.getInstance()
+
+        btnSignUp.setOnClickListener { signupUser() }
+
+        btnGoogleSignUp.setOnClickListener { googleSignupUser() }
+
+
+
+
+    }
+
+    private fun googleSignupUser() {
+        val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.webclient_id))
+            .requestEmail()
+            .build()
+
+        val signInClient = GoogleSignIn.getClient(this,options)
+        signInClient.signInIntent.also {
+            startActivityForResult(it, GOOGLE_SIGN_IN_REQUEST_CODE)
+        }
+    }
+
+
+    private fun signupUser() {
+        val email = etEmail.editText?.text.toString()
+        val password = etPassword.editText?.text.toString()
+
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.createUserWithEmailAndPassword(email,password).await()
+                    withContext(Dispatchers.Main) {
+                    Toast.makeText(this@SignupActivity,"User created successfully!",Toast.LENGTH_LONG).show() }
+                } catch(e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@SignupActivity,e.message,Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode== GOOGLE_SIGN_IN_REQUEST_CODE) {
+            val account = GoogleSignIn.getSignedInAccountFromIntent(data).result
+            account?.let {
+                googleAuthForFirebase(it)
+            }
+        }
+    }
+
+    private fun googleAuthForFirebase(account: GoogleSignInAccount) {
+        val credentials = GoogleAuthProvider.getCredential(account.idToken,null)
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                auth.signInWithCredential(credentials).await()
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@SignupActivity,"Account created successfully!",Toast.LENGTH_LONG).show()
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@SignupActivity,e.message,Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+}
+}
